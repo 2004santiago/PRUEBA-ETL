b388ee5812fa89865fadda4ba0453c636f5f8f43
2004santiago <santiago.gomez.castro.04@gmail.com>
2024-07-31 19:32:31 -0500

    ADD division

--- main.py
+++ main.py
@@ -16,5 +16,17 @@ def suma(a, b):
     return a + b
 
 
+def division(a,b):
+    """
+    This function takes two numbers, `a` and `b`, and returns their division.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    
+    Returns:
+    int or float: The division of `a` and `b`.
+    """
+    return a / b
 
 
