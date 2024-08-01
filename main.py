8fa1ebee8018efd8221f546ee31a5a8e2ccaa0d2
2004santiago <santiago.gomez.castro.04@gmail.com>
2024-07-31 19:26:53 -0500

    Creacion de suma

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Test file
+"""
+
+def suma(a, b):
+    """
+    This function takes two numbers, `a` and `b`, and returns their sum.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    
+    Returns:
+    int or float: The sum of `a` and `b`.
+    """
+    return a + b
+
+
+
+
