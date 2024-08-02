d2cebcf07545478b7dcb476621896fa6e044138b
2004santiago <santiago.gomez.castro.04@gmail.com>
2024-08-02 17:38:52 -0500

    Borrando

--- main.py
+++ main.py
@@ -16,31 +16,8 @@ def suma(a, b):
     return a + b
 
 
-def division(a,b):
-    """
-    This function takes two numbers, `a` and `b`, and returns their division.
-    
-    Parameters:
-    a (int or float): The first number.
-    b (int or float): The second number.
-    
-    Returns:
-    int or float: The division of `a` and `b`.
-    """
-    return a / b
 
-def multiplicar(a,b):
-    """
-    This function takes two numbers, `a` and `b`, and returns their multiplication.
-    
-    Parameters:
-    a (int or float): The first number.
-    b (int or float): The second number.
-    
-    Returns:
-    int or float: The multiplication of `a` and `b`.
-    """
-    return a * b
+
 
 
 
